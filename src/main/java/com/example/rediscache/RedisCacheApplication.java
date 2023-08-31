71871d6e61fb910f08a3c4b8e6f41187c07de0ad
ka <geklbery@gmail.com>
2023-08-31 16:33:30 +1000

    Server port changed to 8081. And minor change

--- src/main/java/com/example/rediscache/RedisCacheApplication.java
+++ src/main/java/com/example/rediscache/RedisCacheApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableCaching
 public class RedisCacheApplication {
 
 	public static void main(String[] args) {
